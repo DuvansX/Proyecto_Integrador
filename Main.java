e8d4e80348ee2c9347e97d539a1fd999e54e2112
DuvansX <duvanchaparro3344@gmail.com>
2026-05-21 13:02:08 -0500

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,17 +1,18 @@
-package proyec;
+
 import java.util.Scanner;
 import java.util.ArrayList;
 
 public class Main {
-
     Scanner scanner = new Scanner(System.in);
     ArrayList<Person> personas = new ArrayList<>();
 
     public static void main(String[] args) {
 
-        Main main = new Main();
-
-        main.showMenu();
+        Main main = new Main(); // Evitar que el menu se cierre con algun error
+        boolean running = true;
+        while (running) {
+            main.showMenu();
+        
 
         int option = main.scanner.nextInt();
         main.scanner.nextLine(); // limpiar buffer
@@ -37,48 +38,57 @@ public class Main {
                 System.out.println("Opción inválida");
         }
     }
+}
 
     public void showMenu() {
         System.out.println("----------- PANEL DE CONTROL -----------");
+        System.out.println("0. Salir");
         System.out.println("1. Crear persona");
         System.out.println("2. Mostrar personas");
         System.out.println("----------------------------------------");
     }
 
     public Person createPerson() {
+        String name;
+        while (true) {
+            System.out.println("Ingrese el nombre de la persona");
+            name = scanner.nextLine();
 
-        System.out.println("Ingrese el nombre de la persona");
-        String name = scanner.nextLine();
+            if (name.matches("[a-zA-ZáéíóúÁÉÍÓÚñÑ ]+")) {
+                break;
+            }
+            System.out.println(" Error: el nombre no puede contener números ni símbolos. Por favor vuelva a ingresar el dato.");
+        }
 
-        System.out.println("Ingrese el documento de la persona");
-        String documentId = scanner.nextLine();
+            System.out.println("Ingrese el documento de la persona");
+            String documentId = scanner.nextLine();
+            
 
-        System.out.println("Ingrese la fecha de nacimiento");
-        String birthDate = scanner.nextLine();
+            System.out.println("Ingrese la fecha de nacimiento");
+            String birthDate = scanner.nextLine();
 
-        System.out.println("Ingrese el género");
-        String gender = scanner.nextLine();
+            System.out.println("Ingrese el género");
+            String gender = scanner.nextLine();
 
-        System.out.println("Ingrese el estado civil");
-        String stateCivil = scanner.nextLine();
+            System.out.println("Ingrese el estado civil");
+            String stateCivil = scanner.nextLine();
 
-        System.out.println("Ingrese el RH");
-        String rh = scanner.nextLine();
+            System.out.println("Ingrese el RH");
+            String rh = scanner.nextLine();
 
-        System.out.println("Ingrese el email");
-        String email = scanner.nextLine();
+            System.out.println("Ingrese el email");
+            String email = scanner.nextLine();
 
-        return new Person(
-                name,
-                documentId,
-                birthDate,
-                gender,
-                stateCivil,
-                rh,
-                email
-        );
+            return new Person(
+                    name,
+                    documentId,
+                    birthDate,
+                    gender,
+                    stateCivil,
+                    rh,
+                    email);
+        }
     }
-}
 
 class Person {
 
@@ -108,7 +118,8 @@ class Person {
         this.email = email;
     }
 
-    @Override
+    // Verificación de cada dato de la persona.
+    @Override  
     public String toString() {
         return "Nombre: " + name +
                 ", Documento: " + documentId +
